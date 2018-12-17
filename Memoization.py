dd9238731dc315d9eea79469688480fabae29007
Henry Lin <henry.lin@appdirect.com>
2018-12-17 06:10:22 -0800

    memoization

---
+++ Memoization.py
@@ -0,0 +1,17 @@
+# Memoization effectively refers to remembering ("memoization" -> "memorandum" -> to be remembered) results of method calls based 
+# on the method inputs and then returning the remembered result rather than computing the result again. You can think of it as a cache
+#  for method results. We'll use this in some of the interview problems as improved versions of a purely recursive solution.
+
+
+# Create cache for known results
+factorial_memo = {}
+
+def factorial(k):
+    
+    if k < 2: 
+        return 1
+    
+    if not k in factorial_memo:
+        factorial_memo[k] = k * factorial(k-1)
+        
+    return factorial_memo[k]
