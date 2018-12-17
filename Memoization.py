ef6554161fcfbbac6584552e4c6d2be451a1f120
Henry Lin <henry.lin@appdirect.com>
2018-12-17 06:11:06 -0800

    memoization v2

--- Memoization.py
+++ Memoization.py
@@ -15,3 +15,24 @@ def factorial(k):
         factorial_memo[k] = k * factorial(k-1)
         
     return factorial_memo[k]
+
+
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+
+
+def factorial(k):
+    
+    if k < 2: 
+        return 1
+    
+    return k * factorial(k - 1)
+
+factorial = Memoize(factorial)
